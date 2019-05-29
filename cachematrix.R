6b6a6b71e5c1d32aa9de0885fd2039939ee32deb
Lawrence Gulliver <techie_fox@hotmail.com>
2019-05-29 16:41:00 +0100

    Populate functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions manage a Matrix with a cache of its inverse state.
 
-## Write a short comment describing this function
+## Creates a Matrix capable of caching its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL    # Inverse cache
+    set <- function(y) {    # Set new value to matrix, delete cache
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x    # Simply returns the value of x
+    
+    setinverse <- function(y) i <<- y
+    getinverse <- function() i
+    
+    list(    # Returns a list of functions to get and set the matrix and its inverse
+         set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse
+    )
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the supplied CacheMatrix. If a cached version is available it will be
+## used, otherwise one will be created.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()    # Returns the current cache
+    if(!is.null(i)) {    # If there is a cache, display a message and return it
+        message("Retrieving from cache")
+        return(i)
+    }
+    
+    data <- x$get()
+    i <- solve(data, ...)    
+    x$setinverse(i)    # Calculate the inverse of the data and save it to the cache, then return it
+    return(i)
 }
